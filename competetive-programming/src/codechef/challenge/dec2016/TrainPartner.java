2ac05c391c22411216a3873d641c61aa629f9f01
neil <neildalvi65@gmail.com>
2017-01-01 20:08:27 +0800

    broken build test

--- competetive-programming/src/codechef/challenge/dec2016/TrainPartner.java
+++ competetive-programming/src/codechef/challenge/dec2016/TrainPartner.java
@@ -1,7 +1,7 @@
 package codechef.challenge.dec2016;
 
 
-import java.io.BufferedReader;
+import java.io.BufferedReader
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
